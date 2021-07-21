25637ceb11d5454106b12c8c59ed479b32838f3a
Guns1301 <67137938+Guns1301@users.noreply.github.com>
2021-07-22 00:12:37 +0300

    ♻️ use hooks

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,22 +1,22 @@
 import PropTypes from "prop-types";
 import style from "./Filter.module.css";
 
-const Filter = ({ filter, getFilterContact }) => (
-  <>
-    <input
-      className={style.filter}
-      type="text"
-      name="filter"
-      value={filter}
-      onChange={getFilterContact}
-      placeholder="Enter name to find"
-    />
-  </>
-);
+export default function Filter({ value, onChange }) {
+  return (
+    <>
+      <input
+        className={style.filter}
+        type="text"
+        name="filter"
+        value={value}
+        onChange={onChange}
+        placeholder="Enter name to find"
+      />
+    </>
+  );
+}
 
 Filter.propTypes = {
-  filter: PropTypes.string,
-  getFilterContact: PropTypes.func,
+  value: PropTypes.string,
+  handleChange: PropTypes.func,
 };
-
-export default Filter;
